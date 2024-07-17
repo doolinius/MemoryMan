eb287ab6be1fadd229e6759223c061993fd83d18
Jeremy Doolin <jdoolin@gmail.com>
2024-07-16 21:06:23 -0400

    Changed title, added screenshot feature

--- main.lua
+++ main.lua
@@ -1,30 +1,34 @@
-require('job')
-require('fixed_partitions')
-require('dynamic_partitions')
+require("job")
+require("fixed_partitions")
+require("dynamic_partitions")
 
 function love.load()
-  math.randomseed(os.time())
-  windowWidth, windowHeight = love.graphics.getDimensions()
-  love.graphics.setBackgroundColor(0.2, 0.2, 0.35, 1)
+	love.window.setTitle("Memory Management Visualization")
+	love.filesystem.setIdentity("MemoryMan")
+	math.randomseed(os.time())
+	windowWidth, windowHeight = love.graphics.getDimensions()
+	love.graphics.setBackgroundColor(0.2, 0.2, 0.35, 1)
 
-  --memory = FP:Create({20, 20, 20, 20, 20, 20}, 'firstFit')
-  --memory = FP:Create({5, 10, 15, 20, 30, 40}, 'bestFit')
-  memory = DynP:Create(120, 'firstFit')
-  j = Job:Create(15, 5)
-  memory:addJobToQueue(j)
+	--memory = FP:Create({20, 20, 20, 20, 20, 20}, 'firstFit')
+	--memory = FP:Create({5, 10, 15, 20, 30, 40}, 'bestFit')
+	memory = DynP:Create(120, "firstFit")
+	j = Job:Create(15, 5)
+	memory:addJobToQueue(j)
 end
 
 function love.update(dt)
-  memory:update(dt)
+	memory:update(dt)
 end
 
 function love.draw()
-  memory:draw()
+	memory:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  if key == 'space' then
-    local j = Job:Create(math.random(2, 35), math.random(4,15))
-    memory:addJobToQueue(j)
-  end
+	if key == "space" then
+		local j = Job:Create(math.random(2, 35), math.random(4, 15))
+		memory:addJobToQueue(j)
+	elseif key == "2" then
+		love.graphics.captureScreenshot(os.time() .. ".png")
+	end
 end
